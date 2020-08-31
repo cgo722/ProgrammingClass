1a6c972b8cfb199b9f4f10273ed4510a8523dd30
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-08-31 14:19:18 -0600

    Class lesson

---
+++ ProgrammingClass/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterMover : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3;
+    public float jumpForce = 10f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+
+        
+        controller.Move(movement *moveSpeed * Time.deltaTime);
+    }
+}
