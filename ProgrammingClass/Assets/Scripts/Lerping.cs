4509281dd62a083ca1267c39dcbcd57cd1b68b34
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-09-16 14:21:11 -0600

    Stuff from the lesson

---
+++ ProgrammingClass/Assets/Scripts/Lerping.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerping : MonoBehaviour
+{
+
+    public Vector3 vOne, vTwo;
+    public float value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = Vector3.Lerp(vOne, vTwo, value);
+        value += 0.1f * Time.deltaTime;
+        transform.position = direction;
+    }
+}
---
+++ ProgrammingClass/Assets/Scripts/MoveLocator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    private Camera cam;
+    public Transform pointObj;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            pointObj.position = hit.point;
+        }
+
+        
+    }
+}
