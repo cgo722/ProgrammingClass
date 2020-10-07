a9c126b843e3317624a8bccc0a85781c364eaf89
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-10-07 14:18:07 -0600

    AI

---
+++ ProgrammingClass/Assets/Scripts/Behaviours/AIBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehavior : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    public bool canNavigate = true;
+    private WaitForFixedUpdate wffu;
+    public float holdTime = 1f;
+    private WaitForSeconds wfs;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private IEnumerator Navigate()
+    {
+        canNavigate = true;
+        yield return wfs;
+        while (canNavigate)
+        {
+            yield return wffu;
+            agent.destination = player.position;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canNavigate = false;
+        StartCoroutine(Navigate());
+    }
+}
