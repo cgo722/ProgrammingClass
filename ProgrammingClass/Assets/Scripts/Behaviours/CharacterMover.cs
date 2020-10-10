2498807f5ec7e00f13def81865e7bf98bd5a288b
Camron Oakeson <Barking_Duuck@yahoo.com>
2020-10-09 19:01:34 -0600

    Downloaded anthony's knockback script and started tweaking it.
    I'm going to try to figure out how to have player get knocked back in contact with player

--- ProgrammingClass/Assets/Scripts/Behaviours/CharacterMover.cs
+++ ProgrammingClass/Assets/Scripts/Behaviours/CharacterMover.cs
@@ -42,7 +42,7 @@ public class CharacterMover : MonoBehaviour
             }
 
             var vInput = Input.GetAxis("Vertical") * moveSpeed.value;
-            //var hInput = Input.GetAxis("Horizontal")*moveSpeed.value;
+
             movement.Set(vInput, yVar, 0);
 
             var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
@@ -66,16 +66,22 @@ public class CharacterMover : MonoBehaviour
             controller.Move((movement) * Time.deltaTime);
         }
     }
-    private IEnumerator KnockBack(ControllerColliderHit hit)
+    private IEnumerator KnockBack(ControllerColliderHit hit, Rigidbody body)
     {
         canMove = false;
         var i = 2f;
-        movement = hit.collider.attachedRigidbody.velocity * i;
+
+        movement = -hit.moveDirection;
+        movement.y = -1;
         while (i > 0)
         {
-            yield return new WaitForFixedUpdate();
+            yield return wffu;
             i -= 0.1f;
             controller.Move((movement) * Time.deltaTime);
+
+            var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+            var forces = pushDir * pushPower;
+            body.AddForce(forces);
         }
         movement = Vector3.zero;
         StartCoroutine(Move());
@@ -97,10 +103,6 @@ public class CharacterMover : MonoBehaviour
             return;
         }
 
-        var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-        var forces = pushDir * pushPower;
-        //body.AddForce(forces);
-        body.velocity = forces;
-        //StartCoroutine(KnockBack(hit));
+        StartCoroutine(KnockBack(hit, body));
     }
 }
