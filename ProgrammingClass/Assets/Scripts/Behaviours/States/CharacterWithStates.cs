30ae218796d4d5f68ac6cb39b34b72e3c19d903b
Camron Oakeson <holy_hades@ymail.com>
2020-10-22 23:48:55 -0600

    Starting import Anthony's state machine but it's not working.
    I'm going to write my own state machiene tomorrow

--- ProgrammingClass/Assets/Scripts/Behaviours/States/CharacterWithStates.cs
+++ ProgrammingClass/Assets/Scripts/Behaviours/States/CharacterWithStates.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 public class CharacterWithStates : MonoBehaviour
 {
     private CharacterController controller;
-    public enum characterStates
-    {
-        StandardWalk,
-        WallCrawl
-    }
-    public characterStates currentCharacterState;
+    public CharacterStateMachineData characterStates;
+
+    private Vector3 movement;
+    public float moveSpeed = 3;
+    public float gravity = -9.81f;
+
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
+
     private void Update()
     {
-        switch (currentCharacterState)
+        var newInput = Input.GetAxis("Vertical") * moveSpeed;
+
+        switch (characterStates.value)
         {
-            case characterStates.StandardWalk:
-                print("Standard Walk");
+            case CharacterStateMachineData.characterStates.StandardWalk:
+                movement.Set(newInput, gravity, 0);
+                print("Walk");
                 break;
-            case characterStates.WallCrawl:
-                print("Wall Crawl");
+            case CharacterStateMachineData.characterStates.NoGravityWalk:
+                movement.Set(newInput, 0, 0);
+                print("Walk");
+                break;
+            case CharacterStateMachineData.characterStates.WallCrawl:
+                movement.Set(0, newInput, 0);
+                print("Crawl");
+                break;
+            case CharacterStateMachineData.characterStates.KnockBack:
+                print("KnockBack");
                 break;
         }
+
+        var newMovement = movement * Time.deltaTime;
+        controller.Move(newMovement);
     }
 }
